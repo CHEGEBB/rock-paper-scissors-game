f21ca9f171b84ebe078160c4c319a51137feba84
CHEGEBB <chegephil24@gmail.com>
2024-06-11 22:32:48 +0300

    added ne components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.scss';
+import Rules from './components/rules';
 
 function App() {
   return (
     <div className="App">
+    <Rules />
     
     </div>
   );
--- src/components/rules.js
+++ src/components/rules.js
@@ -1,6 +1,10 @@
+import React from 'react';
+import '../sass/rules.scss';
+
+
 const Rules = () => {
     return ( 
-        <div>
+        <div className="Rules-container">
             <h1>Rules</h1>
         </div>
      );
