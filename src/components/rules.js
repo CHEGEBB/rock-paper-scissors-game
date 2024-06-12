ad914f724d85bf81f9b4a2f230bd40f06b688829
CHEGEBB <chegephil24@gmail.com>
2024-06-12 11:13:06 +0300

    new

--- src/components/rules.js
+++ src/components/rules.js
@@ -1,36 +1,38 @@
-import React from 'react';
-import '../sass/rules.scss';
-import Paper from '../images/icon-paper.svg';
-import  Scissors from '../images/icon-scissors.svg';
-import Rock from '../images/icon-rock.svg';
-
+import React from "react";
+import "../sass/rules.scss";
+import Paper from "../images/icon-paper.svg";
+import Scissors from "../images/icon-scissors.svg";
+import Rock from "../images/icon-rock.svg";
 
 const Rules = () => {
-    return ( 
-        <div className="Rules-container">
-        <div className="rules">
+  return (
+    <div className="Rules-container">
+      <div className="rules">
         <h1>Rules</h1>
         <div className="rules-content">
+          <div className="row1">
             <div className="paper">
-                <div className="hand">
-<img src={Paper} alt='paper' />
-                </div>
+              <div className="hand">
+                <img src={Paper} alt="paper" />
+              </div>
             </div>
             <div className="scissors">
-            <div className="scissor">
-                    <img src={Scissors} alt='paper' />
-                </div>
+              <div className="scissor">
+                <img src={Scissors} alt="paper" />
+              </div>
             </div>
+          </div>
+          <div className="row2">
             <div className="rock-content">
-                <div className="rock">
-                    <img src={Rock} alt='paper' />
-                </div>
+              <div className="rock">
+                <img src={Rock} alt="paper" />
+              </div>
             </div>
+          </div>
         </div>
-        </div>
-            
-        </div>
-     );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Rules;
