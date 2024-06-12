05e2225d4f8b16471e21169f82dc0aee3d108087
CHEGEBB <chegephil24@gmail.com>
2024-06-12 09:10:15 +0300

    new

--- src/components/rules.js
+++ src/components/rules.js
@@ -7,6 +7,11 @@ const Rules = () => {
         <div className="Rules-container">
         <div className="rules">
         <h1>Rules</h1>
+        <div className="rules-content">
+            <div className="paper"></div>
+            <div className="scissors"></div>
+            <div className="rock"></div>
+        </div>
         </div>
             
         </div>
