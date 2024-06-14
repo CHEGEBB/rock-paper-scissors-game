e7c4f0e92c5f7d4aa8a8f939075c7c57b7dc82a0
CHEGEBB <chegephil24@gmail.com>
2024-06-14 12:15:10 +0300

    added toggle rules logic

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import './App.scss';
 import Rules from './components/rules';
 import Header   from './components/header';
 import Game from './components/game';
+import {useState} from 'react';
 
 function App() {
+
+  const [showRules, setShowRules] = useState(false);
+  
+  const handleToggle= (rules) =>{
+    setShowRules(!showRules);
+  }
+
   return (
     <div className="App">
     <div className="header-section">
     <Header />
-    </div>
+    </div> 
     <div className="game-content">
     <Game/>
-      {/* <Rules /> */}
+
+    {showRules && (
+      <div className="rules">
+      <Rules />
+      </div>
+    )}
 
       <div className="rules-toggle-button">
-        <button>Rules</button>
+        <button onClick={handleToggle}>Rules</button>
       </div>
     </div>
     
