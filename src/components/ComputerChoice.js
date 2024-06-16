b84010deee78f28fb2b4c19670f01391c7b42985
CHEGEBB <chegephil24@gmail.com>
2024-06-16 17:20:32 +0300

    created separate computer component

---
+++ src/components/ComputerChoice.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import PaperComponent from '../components/paper';
+import ScissorsComponent from '../components/scissors';
+import RockComponent from '../components/rock';
+
+const ComputerChoice = ({ selectedChoice }) => {
+  const [computerChoice, setComputerChoice] = useState(null);
+
+  useEffect(() => {
+    if (selectedChoice) {
+      setTimeout(() => {
+        const randomInt = getRandomInt(3);
+        setComputerChoice(randomInt);
+      }, 3000);
+    }
+  }, [selectedChoice]);
+
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  const renderComputerComponent = () => {
+    switch (computerChoice) {
+      case 0:
+        return <PaperComponent />;
+      case 1:
+        return <ScissorsComponent />;
+      case 2:
+        return <RockComponent />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="computer-choice">
+      {computerChoice !== null && renderComputerComponent()}
+    </div>
+  );
+};
+
+export default ComputerChoice;
